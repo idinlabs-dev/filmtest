606ab68fc81b245899e3617e6a24bdbc8c4f8520
Idin Iskandar <idiniskandar.tech@gmail.com>
2026-08-25 15:17:22 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'theme/app_theme.dart';
+import 'providers/drama_provider.dart'
+import 'screens/home_screen.dart';
 
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const ChineseDramaApp());
+}
+
+class ChineseDramaApp extends StatelessWidget {
+  const ChineseDramaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => DramaProvider()),
+      ],
+      child: MaterialApp(
+        title: 'DramaChina',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        themeMode: ThemeMode.system,
+        home: const HomeScreen(),
+      ),
+    );
+  }
+}
